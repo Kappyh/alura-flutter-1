10af54995063a616ed6861c7dc1c324e18143120
Gabriela Menddes <contato@gabrielamendes.dev>
2023-01-03 21:47:53 -0300

    Aula 1 - Modulo 2 flutter
    - Tela de formulario

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
-import 'package:alura_flutter_curso_1/screens/initial_screen.dart';
+import 'package:alura_flutter_curso_1/screens/form_screen.dart';
+// import 'package:alura_flutter_curso_1/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }
---
+++ lib/screens/form_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nova tarefa'),
+      ),
+        body: Center(
+          child: Container(
+            height: 650,
+            width: 375,
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all()
+            ),
+            child: Column()
+          ),
+        )
+    );
+  }
+}
