f95ce94909f94c3a001d26430b3693b40ee52a18
Gabriela Menddes <contato@gabrielamendes.dev>
2023-01-04 21:47:17 -0300

    Aula 1 Formulários - modulo 2
    - Adiciona inputs para inserir textos e link da imagem
    - Adiciona campo para preview da imagem
    - Adiciona validação para existencia da imagem no campo
    de preview
    - Adiciona espaço entre os campos do formulario

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -7,8 +7,12 @@ class FormScreen extends StatefulWidget {
   State<FormScreen> createState() => _FormScreenState();
 }
 
-class _FormScreenState extends State<FormScreen> with SingleTickerProviderStateMixin {
+class _FormScreenState extends State<FormScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController difficultyController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
 
   @override
   void initState() {
@@ -25,21 +29,94 @@ class _FormScreenState extends State<FormScreen> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Nova tarefa'),
-      ),
+        appBar: AppBar(
+          title: const Text('Nova tarefa'),
+        ),
         body: Center(
           child: Container(
-            height: 650,
-            width: 375,
-            decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all()
-            ),
-            child: Column()
-          ),
-        )
-    );
+              height: 650,
+              width: 375,
+              decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all()),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: nameController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Nome',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: difficultyController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Dificuldade',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: imageController,
+                      onChanged: (text)=>{
+                        setState((){
+                        })
+                      },
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Imagem',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        width: 2.0,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        imageController.text,
+                        errorBuilder: (BuildContext context, Object exception, StackTrace? stacktrace){
+                          return Image.asset('assets/images/nophoto.png');
+                        },
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print(nameController.text);
+                    },
+                    child: Text('Adicionar'),
+                  )
+                ],
+              )),
+        ));
   }
 }
